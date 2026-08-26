2d2c90046805e8c72e996d4a0ee72d7f76a60ec7
T. T. <takikawatouma@gmail.com>
2026-08-26 16:14:10 +0900

    Update postlistjs.js

--- postlistjs.js
+++ postlistjs.js
@@ -1,33 +1,15 @@
-const articleht = document.getElementById("postarticle");
-const pnbutton = document.getElementById("prevnnext");
-
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-const datehpn = urlParams.get('date');
-
-fetch("blog/" + datehpn ".html")
-    .then(response => response.text())
-    .then(text => {
-        console.log(text);
-        articleht.innerHTML += datehpn
-        articleht.innerHTML += text
-    })
+const postList = document.getElementById("post-list");
 
 fetch("posts.txt")
     .then(response => response.text())
     .then(text => {
-        let herewhere = text.indexOf(date)
-        let pprevwhere = text.lastIndexOf("\n", herewhere)
-        let prevwhere = text.lastIndexOf("\n", pprevwhere)
-        let pnextwhere = text.IndexOf("\n", herewhere)
-        let nextwhere = text.IndexOf("\n", pnextwhere)
-        //・・番目の文字って言おうのが返り値。prevwhere番目からnextwhere番目の文字列をpnbuttonに出したらいい。リンク付もしないと。
-        let prevlineouttext = text.substring(prevwhere, pprevwhere)
-        console.log(prevlineouttext)
-        let nextlineouttext = text.substring(nextwhere, pnextwhere)
-        console.log(nextlineouttext)
-        let prevlineoutsplit = prevlineouttext.split("|");
-        let nextlineoutsplit = nextlineouttext.split("|");
-        pnbutton.innerHTML += "Previous post <a href=\"postviewer.html?date=" + prevlineoutsplit[0] "\">" + prevlineouttext + "</a>"
-        pnbutton.innerHTML += "Next post <a href=\"postviewer.html?date=" + nextlineoutsplit[0] "\">" + nextlineouttext + "</a>"
+        console.log(text);
     })
+    .then(txt => {
+        let linelist = txt.split(/\r\n|\n/);
+        for (let line of linelist) {
+            let plslist = line.split("|");
+            let date = plslist[0]
+            document.getElementById("post-list").innerHTML = '<a href="posts/' + date + '.html">' + plslist[0] + "|" + plslist[1] + "</a>";
+        }
+    });
