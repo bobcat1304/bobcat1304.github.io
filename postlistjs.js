317504c6d5888d2dba857f0d472a800a3231b7e6
T. T. <takikawatouma@gmail.com>
2026-08-26 16:21:30 +0900

    Update postlistjs.js

--- postlistjs.js
+++ postlistjs.js
@@ -1,15 +1,14 @@
 const postList = document.getElementById("post-list");
-
+postList.innerHTML += "<ul>"
 fetch("posts.txt")
     .then(response => response.text())
     .then(text => {
         console.log(text);
-    })
-    .then(txt => {
-        let linelist = txt.split(/\r\n|\n/);
+        let linelist = text.split(/\r\n|\n/);
         for (let line of linelist) {
             let plslist = line.split("|");
             let date = plslist[0]
-            document.getElementById("post-list").innerHTML = '<a href="posts/' + date + '.html">' + plslist[0] + "|" + plslist[1] + "</a>";
+            postList.innerHTML += '<li><a href="posts/' + date + '.html">' + plslist[0] + "|" + plslist[1] + "</a></li><br>";
         }
-    });
+        postList.innerHTML += "</ul>"
+    })
